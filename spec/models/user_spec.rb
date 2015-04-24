6223790763c99ec730c6d951f4f208adbf8bfdcd
brandon jones <brandon.jones9@gmail.com>
2015-04-23 22:10:17 -0700

    user auth
    devise auth seems to be setup at the most basic level. User model testing (no devise testing) is setup.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,24 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "should have an email" do
-    user = Factory.new(:user, password: '')
+    user = FactoryGirl.build(:user, email: '')
+    expect(user.save).to be_falsy
+    user.update_attribute(:email, 'bob@bob.com')
+    expect(user.save).to be_truthy
+  end
 
+  it "should have a unique email" do
+    user = FactoryGirl.create(:user)
+    user2 = FactoryGirl.build(:user, email: user.email)
+    expect(user2.save).to be_falsy
+    user2.update_attribute(:email, 'i@am.email')
+    expect(user2.save).to be_falsy
+  end
+
+  it "should have a password" do
+    user = FactoryGirl.build(:user, password: '')
+    expect(user.save).to be_falsy
+    user.update_attribute(:password, 'password')
+    expect(user.save).to be_truthy
   end
 end
