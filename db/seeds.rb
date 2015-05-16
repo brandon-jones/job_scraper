8584829472d44402d76e3533303b7175d1f031d2
brandon jones <brandon.jones9@gmail.com>
2015-05-15 21:54:34 -0700

    see for both dbs

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# JobSearch.create(name: 'We Work Remotely', 
-#                  description: 'Remote only Jobs', 
-#                  image: 'https://weworkremotely.com/images/header.jpg', 
-#                  options: "{\"categories\":[[\"design\",\"1\"],[\"programming\",\"2\"],[\"buisness exec\",\"3\"],[\"miscellaneous\",\"4\"],[\"copywriter\",\"5\"],[\"sys admin\",\"6\"],[\"customer service/support\",\"7\"]],\"search terms\":\"\"}",
-#                  homepage: 'https://weworkremotely.com', 
-#                  search_url: 'https://weworkremotely.com/jobs/search?term={{search_terms}}',
-#                  type: 'WeWorkRemotely')
+JobSearch.create(name: 'We Work Remotely', 
+                 description: 'Remote only Jobs', 
+                 image: 'https://weworkremotely.com/images/header.jpg', 
+                 options: "{\"categories\":[[\"design\",\"1\"],[\"programming\",\"2\"],[\"buisness exec\",\"3\"],[\"miscellaneous\",\"4\"],[\"copywriter\",\"5\"],[\"sys admin\",\"6\"],[\"customer service/support\",\"7\"]],\"search terms\":\"\"}",
+                 homepage: 'https://weworkremotely.com', 
+                 search_url: 'https://weworkremotely.com/jobs/search?term={{search_terms}}',
+                 type: 'WeWorkRemotely')
 
 JobSearch.create(name: 'Jr Dev Jobs', 
                  description: 'Junior only jobs', 
